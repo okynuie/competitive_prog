5be3e3aaee0a02258d37d3e9522ac3e23938cee2
oky nuie <nuieden4pster@gmail.com>
2021-08-01 19:00:41 +0800

    trying to solve waytoolongwords problemset

---
+++ waytoolongwords.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    char y;
+
+    input word;
+    print line n;
+
+    length_w = length.word;
+    if(length_w > n && length_w < 100){
+        nilai tengah = length_w - 2;
+        hasil = str 0 + tengah + str length_w;
+
+    }else{
+        hasil = word;
+    }
+
+
+    printf("Hello World cpp vscode");
+    return 0;
+}
